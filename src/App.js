f8365cf5f776ca6290cd4f4273629b8d6aa191e5
Brandon-Abundis <abrandon99@gmail.com>
2024-01-31 09:35:43 -0700

    Added multiplepages

--- src/App.js
+++ src/App.js
@@ -1,80 +1,22 @@
-import React from 'react';
-import './App.css';
-import { useState, useEffect } from 'react';
-import {
-  ref,
-  uploadBytes,
-  getDownloadURL,
-  listAll,
-  list,
-} from 'firebase/storage';
-import { storage } from './backend/firebase';
-import { v4 } from 'uuid';
+import React from "react";
+import Layout from "./page/Layout";
+import Login from "./page/Login";
+import Signup from "./page/Signup";
+import Profile from "./page/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [imageUpload, setImageUpload] = useState(null);
-  const [fileURLs, setFileURLs] = useState([]);
-
-  const imagesListRef = ref(storage, 'images/');
-  
-  const uploadFile = () => {
-    if (imageUpload == null) return; // Didn't select any images
-
-    // getting a reference to from firebase, and a filepath with filename.
-    // the filename has a uuid function to add a random number at the end.
-
-    let arr = imageUpload.name.split(".");
-    // const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    const imageRef = ref(storage, `images/${arr[0]+v4() +"."+arr[1]}`);
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setFileURLs((prev) => [...prev, url]).then(() => {
-          console.log(prev)
-          alert('File Uploaded');
-        });
-      });
-    });
-    console.log(imageRef)
-  };
-  
-
-  // Get all the items in the storage reference, then loop through
-  // each item and grab them. Then get the url based on that current
-  // item, then to add to an item to a list that is a state, you get
-  // the current value of the list then set it equal to the same list
-  // but add the new url at the end.
-  useEffect(() => {
-    
-    const fetchImages = async () => {
-
-      let result = await listAll(imagesListRef);
-      let urlPromises = result.items.map((imageRef) => getDownloadURL(imageRef))
-      return Promise.all(urlPromises);
-    };
-
-    const loadImages = async () => {
-      const urls = await fetchImages();
-      setFileURLs(urls);
-    };
-    loadImages();
-  }, []);
-
+function App(){
   return (
-    <div className="App">
-      <input
-        type="file"
-        onChange={(event) => {
-          setImageUpload(event.target.files[0]);
-        }}
-      />
-      <button onClick={uploadFile}> Upload Image</button>
-      {
-      fileURLs.map((url) => {
-        return <img src={url} />;
-      })
-      }
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path = "/" element = { <Layout></Layout> }>
+            <Route index element = { <Login></Login> }></Route>
+            <Route path = "/signup" element = { <Signup></Signup> } ></Route>
+            <Route path = "/profile" element = { <Profile></Profile> }></Route>
+          </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
